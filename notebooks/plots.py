ccb7cd04557b8be129d30f0ded056c24d96cc4a7
thiago rissi <thiago.riss@usp.br>
2024-08-23 20:45:06 +0000

    Add documentation for plots.py

--- notebooks/plots.py
+++ notebooks/plots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy.typing as npt
 import numpy as np
+import pathlib
 
 
 def compare_imputation(
@@ -12,7 +13,22 @@ def compare_imputation(
     ts_id: int,
     ylabel: str = "Time Serie",
     xlabel: str = "Time",
-):
+) -> None:
+    """
+    Compare the original, missing, and imputed time series data.
+
+    Parameters:
+        x_nan (npt.NDArray): The array containing the missing values.
+        x_imp (npt.NDArray): The array containing the imputed values.
+        x (npt.NDArray): The array containing the original values.
+        title (str): The title of the plot.
+        ts_id (int): The index of the time series to compare.
+        ylabel (str, optional): The label for the y-axis. Defaults to "Time Serie".
+        xlabel (str, optional): The label for the x-axis. Defaults to "Time".
+
+    Returns:
+        None
+    """
     fig, axes = plt.subplots(3, 1, figsize=(6, 10), sharex=True)
     fig.suptitle(title)
     t = np.arange(x.shape[0])
@@ -43,6 +59,19 @@ def plot_metric(
     title: str = "",
     save_name: str = "output",
 ) -> None:
+    """
+    Plot a metric against missing percentage.
+
+    Args:
+        results (dict[str, list]): A dictionary containing the results for different names.
+        metric (str): The metric to be plotted.
+        pmiss (list, optional): The missing percentage values. Defaults to [0, 20, 40, 60, 70, 80, 90].
+        title (str, optional): The title of the plot. Defaults to "".
+        save_name (str, optional): The name of the output file. Defaults to "output".
+
+    Returns:
+        None
+    """
     fig, ax = plt.subplots(figsize=(6, 6))
     for name, result in results.items():
         ax.plot(pmiss, result, label=name)
@@ -59,4 +88,5 @@ def plot_metric(
         ncol=2,
     )
     plt.tight_layout()
+    pathlib.Path("../notebooks/figures").mkdir(parents=True, exist_ok=True)
     plt.savefig(f"../notebooks/figures/{save_name}.png")
